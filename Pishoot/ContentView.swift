6d7dd5f9a5f20e1270a52043d0a0ebf7493ea907
ridhoafwani <zikrurridhoafwani@gmail.com>
2024-06-25 15:15:18 +0700

    Initial Commit

---
+++ Pishoot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pishoot
+//
+//  Created by Muhammad Zikrurridho Afwani on 25/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pishoot/PishootApp.swift
@@ -0,0 +1,17 @@
+//
+//  PishootApp.swift
+//  Pishoot
+//
+//  Created by Muhammad Zikrurridho Afwani on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PishootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
