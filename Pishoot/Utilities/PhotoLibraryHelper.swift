05db0018d14f12c0dce0162a6f8e3eb7adeea9d1
ridhoafwani <zikrurridhoafwani@gmail.com>
2024-07-02 08:29:49 +0700

    refactored

--- Pishoot/Utilities/PhotoLibraryHelper.swift
+++ Pishoot/Utilities/PhotoLibraryHelper.swift
@@ -1,61 +1,9 @@
-////
-////  PhotoLibraryHelper.swift
-////  Pishoot
-////
-////  Created by Muhammad Zikrurridho Afwani on 25/06/24.
-////
 //
-//import UIKit
-//import Photos
+//  PhotoLibraryHelper.swift
+//  Pishoot
 //
-//struct PhotoLibraryHelper {
-//    static func requestPhotoLibraryPermission(completion: @escaping (Bool) -> Void) {
-//            PHPhotoLibrary.requestAuthorization { status in
-//                DispatchQueue.main.async {
-//                    completion(status == .authorized)
-//                }
-//            }
-//        }
-//    
-//    static func openPhotosApp() {
-//        if let url = URL(string: "photos-redirect://") {
-//            if UIApplication.shared.canOpenURL(url) {
-//                UIApplication.shared.open(url, options: [:], completionHandler: nil)
-//            }
-//        }
-//    }
-//    
-//    static func fetchLastPhoto(completion: @escaping (UIImage?) -> Void) {
-//        PHPhotoLibrary.requestAuthorization { status in
-//            if status == .authorized {
-//                let fetchOptions = PHFetchOptions()
-//                fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
-//                fetchOptions.fetchLimit = 1
-//                let fetchResult = PHAsset.fetchAssets(with: .image, options: fetchOptions)
-//                
-//                if let asset = fetchResult.firstObject {
-//                    let imageManager = PHImageManager.default()
-//                    let options = PHImageRequestOptions()
-//                    options.deliveryMode = .highQualityFormat
-//                    options.isSynchronous = true
-//                    imageManager.requestImage(for: asset, targetSize: CGSize(width: 50, height: 50), contentMode: .aspectFill, options: options) { image, _ in
-//                        DispatchQueue.main.async {
-//                            completion(image)
-//                        }
-//                    }
-//                } else {
-//                    DispatchQueue.main.async {
-//                        completion(nil)
-//                    }
-//                }
-//            } else {
-//                DispatchQueue.main.async {
-//                    completion(nil)
-//                }
-//            }
-//        }
-//    }
-//}
+//  Created by Muhammad Zikrurridho Afwani on 25/06/24.
+//
 
 import UIKit
 import Photos
--- Pishoot/Views/CameraPreviewView.swift
+++ Pishoot/Views/CameraPreviewView.swift
@@ -1,86 +1,59 @@
-////
-////  CameraPreviewView.swift
-////  Pishoot
-////
-////  Created by Muhammad Zikrurridho Afwani on 25/06/24.
-////
 //
-//import SwiftUI
-//import AVFoundation
+//  CameraPreviewView.swift
+//  Pishoot
 //
-//struct CameraPreviewView: UIViewRepresentable {
-//    class CameraPreview: UIView {
-//        var previewLayer: AVCaptureVideoPreviewLayer
-//        
-//        override init(frame: CGRect) {
-//            previewLayer = AVCaptureVideoPreviewLayer()
-//            super.init(frame: frame)
-//            layer.addSublayer(previewLayer)
-//        }
-//        
-//        required init?(coder: NSCoder) {
-//            fatalError("init(coder:) has not been implemented")
-//        }
-//        
-//        override func layoutSubviews() {
-//            super.layoutSubviews()
-//            previewLayer.frame = bounds
-//        }
-//    }
-//    
-//    var session: AVCaptureSession
-//    
-//    func makeUIView(context: Context) -> CameraPreview {
-//        let view = CameraPreview()
-//        view.previewLayer.session = session
-//        view.previewLayer.videoGravity = .resizeAspectFill
-//        return view
-//    }
-//    
-//    func updateUIView(_ uiView: CameraPreview, context: Context) {}
-//}
+//  Created by Muhammad Zikrurridho Afwani on 25/06/24.
+//
 
-//import SwiftUI
-//import AVFoundation
-//
-//struct CameraPreviewView: UIViewRepresentable {
-//    class CameraPreview: UIView {
-//        var previewLayer: AVCaptureVideoPreviewLayer
-//        
-//        override init(frame: CGRect) {
-//            previewLayer = AVCaptureVideoPreviewLayer()
-//            super.init(frame: frame)
-//            layer.addSublayer(previewLayer)
-//        }
-//        
-//        required init?(coder: NSCoder) {
-//            fatalError("init(coder:) has not been implemented")
-//        }
-//        
-//        override func layoutSubviews() {
-//            super.layoutSubviews()
-//            previewLayer.frame = bounds
-//        }
-//    }
-//    
-//    var session: AVCaptureMultiCamSession
-//    
-//    func makeUIView(context: Context) -> CameraPreview {
-//        let view = CameraPreview()
-//        view.previewLayer.session = session
-//        view.previewLayer.videoGravity = .resizeAspectFill
-//        
-//        // Set up the preview layer to show only the wide-angle camera
-//        if let wideAngleInput = session.inputs.first(where: { ($0 as? AVCaptureDeviceInput)?.device.deviceType == .builtInWideAngleCamera }) as? AVCaptureDeviceInput,
-//           let port = wideAngleInput.ports.first(where: { $0.mediaType == .video }) {
-//            let connection = AVCaptureConnection(inputPort: port, videoPreviewLayer: view.previewLayer)
-//            if session.canAddConnection(connection) {
-//                session.addConnection(connection)
-//            }
-//        }
-//        
-//        return view
-//    }
-//    
-//    func updateUIView(_ uiView: CameraPreview, context: Context) {}
-//}
+import SwiftUI
+import AVFoundation
+
+
+struct CameraPreviewView: UIViewRepresentable {
+    class CameraPreview: UIView {
+        var previewLayer: AVCaptureVideoPreviewLayer
+        
+        override init(frame: CGRect) {
+            previewLayer = AVCaptureVideoPreviewLayer()
+            super.init(frame: frame)
+            layer.addSublayer(previewLayer)
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        override func layoutSubviews() {
+            super.layoutSubviews()
+            previewLayer.frame = bounds
+        }
+    }
+    
+    var session: AVCaptureSession
+    
+    func makeUIView(context: Context) -> CameraPreview {
+        let view = CameraPreview()
+        view.previewLayer.session = session
+        view.previewLayer.videoGravity = .resizeAspectFill
+
+        if let wideAngleInput = session.inputs.first(where: { ($0 as? AVCaptureDeviceInput)?.device.deviceType == .builtInWideAngleCamera }) as? AVCaptureDeviceInput,
+           let port = wideAngleInput.ports.first(where: { $0.mediaType == .video }) {
+            let connection = AVCaptureConnection(inputPort: port, videoPreviewLayer: view.previewLayer)
+            if session.canAddConnection(connection) {
+                session.addConnection(connection)
+            }
+            
+            do {
+                try wideAngleInput.device.lockForConfiguration()
+                wideAngleInput.device.videoZoomFactor = 1.0
+                wideAngleInput.device.unlockForConfiguration()
+            } catch {
+                print("Error setting initial zoom factor: \(error)")
+            }
+        }
+
+        return view
+    }
+    
+    func updateUIView(_ uiView: CameraPreview, context: Context) {}
+}
