0903559af72d080786dae752e6d0f4cf7abfb204
ridhoafwani <zikrurridhoafwani@gmail.com>
2024-07-05 23:59:52 +0700

    featonintall(onboarding): add onboarding

--- Pishoot/PishootApp.swift
+++ Pishoot/PishootApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct PishootApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            OnBoardingContainerView()
         }
     }
 }
